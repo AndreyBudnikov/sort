67a083060e38c2a0f9b7a094eac528f9d5c328c6
AndreyBudnikov <59439128+AndreyBudnikov@users.noreply.github.com>
2020-01-15 11:42:19 +0300

    UPDATE

--- sort/main.cpp
+++ sort/main.cpp
@@ -213,71 +213,35 @@ uint MyQuickSort(Intuha<T>* obj, int start, int stop)
 	return 0;
 }
 
-template<class T>
-uint SpecialSort(Intuha<T>* obj, uint start, uint len)
-{
-	return 0;
-}
+const uint MAX_SIGN = 10000;
+inline uint Prepare(float data) { return MAX_SIGN; }
+inline uint Prepare(int data)   { return 1; }
 
 template<class T>
-typename enable_if<is_same<T, int>::value, T>::type
-SortSpecial(Intuha<int>* obj, int val, uint& count, const uint len)
-{
-	for (uint j = 0; j < len; j++)
-	{
-		obj[count++] = val;
-	}
-	return 0;
-}
-
-template<class T>
-typename enable_if<is_same<T, float>::value || is_same<T, double>::value, T>::type
-SortSpecial(Intuha<T>* obj, int val, uint& count, const uint len)
+uint SortCalc(Intuha<T>* obj, int start, int len)
 {
 	assert(len > 0);
-	T intpath;
-	uint max = static_cast<T>(modf(obj[count].GetValue(), &intpath) * 1000), tmp;
-	for (uint i = 0; i < len; i++)
-	{
-		tmp = static_cast<T>(modf(obj[i].GetValue(), &intpath) * 1000);
-		if (max < tmp) { max = tmp; }
-	}
-	auto ptr = make_unique<uint[]>(++max);
-	for (int i = 0; i < max; i++) { ptr[i] = static_cast<int>(0); }
-	uint test;
-	for (int i = 0; i < len; i++) 
-	{ 
-		test = modf(obj[count + i].GetValue(), &intpath) * 1000;
-		ptr[static_cast<uint>(modf(obj[count + i].GetValue(), &intpath) * 1000)]++; 
-	}
-	for (uint i = 0; i < max; i++)
-	{
-		for (uint j = 0; j < ptr[i]; j++)
-		{
-			obj[count++] = val;
-		}
-	}
-	return 0;
-}
+	const uint factor = Prepare(static_cast<T>(0));
+	uint tmp;
 
-template<class T>
-uint SortCalc(Intuha<T>* obj, int start, int stop)
-{
-	assert(stop > start);
-	int max = static_cast<T>(obj[start].GetValue());
-	for (uint i = start + 1; i <= stop; i++)
+	int max = static_cast<T>(obj[start].GetValue() * factor);
+	for (uint i = 1; i < len; i++)
 	{
-		if (max < obj[i].GetValue()) { max = static_cast<uint>(obj[i].GetValue()); }
+		tmp = obj[start + i].GetValue() * factor;
+		if (max < tmp) { max = tmp; }
 	}
 
 	auto ptr = make_unique<uint[]>(++max);
 	for (int i = 0; i < max; i++) { ptr[i] = static_cast<uint>(0); }
-	for (int i = start; i <= stop; i++) { ptr[static_cast<uint>(obj[i].GetValue())]++; }
+	for (int i = 0; i < len; i++) { ptr[static_cast<uint>(obj[start + i].GetValue() * factor)]++; }
 
 	uint count = start;
 	for (int i = 0; i < max; i++)
 	{
-		if (ptr[i] > 0) { SortSpecial<T>(obj, i, count, ptr[i]); }
+		for (uint j = 0; j < ptr[i]; j++)
+		{
+			obj[count++] = static_cast<T>(i) / factor;
+		}
 	}
 	return 0;
 }
@@ -285,23 +249,24 @@ uint SortCalc(Intuha<T>* obj, int start, int stop)
 
 uint Binary(multiset<Intuha<uint>>& obj, uint start, uint stop)
 {
-	cout << "Enter Bin" << endl;
+	return 0;
+}
 
-	//obj.
+template<class T>
+Intuha<T>* Merge(Intuha<T>* obj, int start, int len)
+{
+	assert(len > 0);
 
-	//..decltype(auto) iter = obj.cbegin();
-	Intuha<uint> tmp;
-	//tmp = *iter;
+	if(len == 1) {return }
+	uint mid = (len + 1) / 2;
+	auto ptr1 = obj;
+	auto ptr2 = obj + mid * sizeof(Intuha<T>);
 
-	cout << "Enter cycle" << endl;
-	//while (tmp != obj.cend())
-	//{
-	//	iter = obj.upper_bound(tmp);
-	//	cout << "val" << (*iter).GetValue() << endl;
-	//	
-	//}
 
-	// return (*iter).GetValue();
+	
+	ptr1 = Merge(obj, start, mid);
+	ptr2 = Merge(obj, start + mid, len - mid);
+	
 	return 0;
 }
 
@@ -312,7 +277,7 @@ uint Valid_Sort(Intuha<T>* obj, uint len)
 		T test_val = obj[0].GetValue();
 		for (uint i = 0; i < len; i++)
 		{
-			cout << obj[i].GetValue() << endl;
+			//cout << obj[i].GetValue() << endl;
 			if (test_val > obj[i].GetValue())
 			{
 				std::cout << "SORT FALSE" << endl;
@@ -347,33 +312,33 @@ int main()
 	auto s2 = make_unique<Intuha<float>[]>(len);
 
 	//int test[] = { 6, 0, 1, 1, 6, 5, 7, 2, 3, 8, 0, 17, 4, 6 };
-	//srand(unsigned(time(0)));
+	srand(unsigned(time(0)));
 
 	std::cout << "Create array" << endl;
 	for (uint i = 0; i < len; i++)
 	{
 		//s1[i] = test[i];
-		s1[i] = static_cast<float>(rand() % 10000) / 1000;
+		s1[i] = static_cast<float>(rand() % 100000) / MAX_SIGN;
 	}
 
 	copyArr(s1.get(), s2.get(), len);
 	{
 		HronoTimer hTimer1("Bubbles");
-	 	//SortBubble(s1.get(), len);
+	 	SortBubble(s2.get(), len);
 	}
 	Valid_Sort(s2.get(), len);
 
 	copyArr(s1.get(), s2.get(), len);
 	{
 		HronoTimer hTimer1("Shake");
-		//SortShake(s2.get(), len);
+		SortShake(s2.get(), len);
 	}
 	Valid_Sort(s2.get(), len);
 
 	copyArr(s1.get(), s2.get(), len);
 	{
 		HronoTimer hTimer1("SortCalc");
-		SortCalc(s2.get(), 0, len - 1);
+		SortCalc(s2.get(), 0, len);
 	}
 	Valid_Sort(s2.get(), len);
 
